fc18c2ed4bb267229f088efde0521853da94b570
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-11-30 14:55:43 +0900

    Add files via upload

---
+++ nitro.py
@@ -0,0 +1,29 @@
+
+###################
+#discord.py==1.4.0#
+###################
+
+import discord
+import random
+import string
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("봇이 성공적으로 실행되었습니다.")
+    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('봉순아 니트로'):
+        ranNitro = ""
+        for i in range(0, 16):
+            ranNitro += str(random.choice(string.ascii_letters))
+            NitroEmbed = discord.Embed(title='니트로 생성기', description='https://discord.gift/' + ranNitro)
+        await message.channel.send(embed=NitroEmbed)
+        
+client.run('★TOKEN★')
